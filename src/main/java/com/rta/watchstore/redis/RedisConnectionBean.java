c4cd44ae6e5d7a88998855bd52ca02bf1f4e03d8
reasonthearchitect <rhys.personal@gmail.com>
2016-07-31 06:54:02 -0400

    Got redis working with the local embedded server and can now ping.

--- src/main/java/com/rta/watchstore/redis/RedisConnectionBean.java
+++ src/main/java/com/rta/watchstore/redis/RedisConnectionBean.java
@@ -1,9 +1,15 @@
 package com.rta.watchstore.redis;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisConnectionBean {
 
-
+    @Bean
+    public RedisConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory("127.0.0.1", 6379);
+    }
 }
---
+++ src/main/java/com/rta/watchstore/redis/StringRedisTemplateBean.java
@@ -0,0 +1,27 @@
+package com.rta.watchstore.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringRedisTemplateBean {
+
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public RedisTemplate<String, String> getStringRestTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.afterPropertiesSet();
+        //System.out.println("&&&&&&&&&&&&&&&&&&&&&   " + redisTemplate.getConnectionFactory().getConnection().ping());
+        return redisTemplate;
+    }
+}
