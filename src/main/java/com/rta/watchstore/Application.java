33ae5f8540a31defaec56ae42c0883fb7df5eb6b
reasonthearchitect <rhys.personal@gmail.com>
2016-07-31 09:12:18 -0400

    Removed unused reference in the Application file.

--- src/main/java/com/rta/watchstore/Application.java
+++ src/main/java/com/rta/watchstore/Application.java
@@ -1,13 +1,10 @@
 package com.rta.watchstore;
 
-import com.rta.watchstore.data.WatchingCar;
 import com.rta.watchstore.generated.config.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 
@@ -18,7 +15,6 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Map;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
