63374a9ff24896b0a5711c7e94baf891a5670c3b
reasonthearchitect <rhys.personal@gmail.com>
2016-08-07 21:07:51 -0400

    Adding configuration service for the watch store

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM frolvlad/alpine-oraclejdk8:slim
 VOLUME /tmp
 ADD build/libs/watchstore-0.0.1-SNAPSHOT.jar app.jar
 RUN sh -c 'touch /app.jar'
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-Dspring.profiles.active=prod","-jar","/app.jar"]
 EXPOSE 8210
--- src/main/java/com/rta/watchstore/Application.java
+++ src/main/java/com/rta/watchstore/Application.java
@@ -82,7 +82,7 @@ public class Application {
         if (!source.containsProperty("spring.profiles.active") &&
                 !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
 
-            app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
+            //app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
         }
     }
 }
